8df4fa725137ce327afb63dd24ab4274518519c7
zhanxq <1074919969@qq.com>
2020-07-26 08:03:54 -0400

    add qa

--- nodetest.js
+++ nodetest.js
@@ -1,38 +1,48 @@
 var express = require('express')
 
+var fortune = require("./lib/fortune.js") 
 var app = express();
 
 var handlebars = require('express3-handlebars')
-		.create({defaultLayout: 'main'});
+  .create({ defaultLayout: 'main' });
 
-app.engine('handlebars',handlebars.engine)
-
-app.set('view engine','handlebars')
-
-app.set('port',process.env.PORT || 3000);
-
-app.get('/',function(req,res){
- res.render('home')
-})
-
-app.get('/about',function(req,res)
-{
-  res.render('about');	
-})
+app.engine('handlebars', handlebars.engine)
 
 app.use(function(req,res,next){
-res.status(404)
-res.render("404")
+  res.locals.showTests = app.get("env") !== "production" && req.query.test === "1";
+  next();
 })
 
-app.use(function(err,req,res,next){
-console.error(err.stack);
-res.status(500);
-res.render('500');
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
 })
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'),function(){
-console.log('nodetest run...')
+app.set('view engine', 'handlebars')
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.get('/about', function (req, res) {
+  res.render('about',{
+    fortune: fortune.getFortune(),
+    pageTestScript:'/qa/tests-about.js'
+  });
+})
+
+app.use(function (req, res, next) {
+  res.status(404)
+  res.render("404")
+})
+
+
+app.listen(app.get('port'), function () {
+  console.log('nodetest run...')
 })
---
+++ qa/tests-about.js
@@ -0,0 +1,5 @@
+suite("'About' page tests",function () {
+    test('page should contain link to contact page',function(){
+        assert($('a[href="/contact"]').length);
+    });
+});
